3c33f432a63121d3b52cc885320399c0be4e6661
Anusha Konatala <anu.konatala@gmail.com>
2018-06-15 18:15:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,13 @@ class Dog
   attr_accessor :name
   def initialize(name)
     @name = name
+    @@all << self
   end
   
   def self.clear_all
-    
+    @@ll.clear
   end
+  
   def self.all
     @all.each do |dog|
       puts dog.name
